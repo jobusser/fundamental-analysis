a9238730223a302efccfa272433ad88b5d5a18dc
Jo Busser <jobusser1@gmail.com>
2024-07-11 21:38:01 +0100

    Handle cookies

--- src/test_scraper.py
+++ src/test_scraper.py
@@ -2,32 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Chrome options
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+from selenium.common.exceptions import TimeoutException
 
-# Initialize the Chrome driver with options
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# Open the Yahoo Finance page for AAPL
-driver.get("https://finance.yahoo.com/quote/AAPL/")
+def handle_cookies(driver):
+    try:
+        # Wait up to 3 seconds for the consent modal to show up
+        consent_overlay = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.consent-overlay')))
 
-# Wait for the page to load completely
-wait = WebDriverWait(driver, 10)
+        # Click the "Accept all" button
+        accept_all_button = consent_overlay.find_element(By.CSS_SELECTOR, '.accept-all')
+        accept_all_button.click()
+    except TimeoutException:
+        print('Cookie consent overlay not found or already accepted')
 
-try:
-    # Example: Extract the stock price
-    stock_price_element = driver.find_elements(By.TAG_NAME, "li")
-    # Print the results
-    print(stock_price_element)
 
-finally:
-    # Close the driver
-    driver.quit()
+if __name__ == '__main__':
+    # Set up Chrome options
+    options = Options()
+    # options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # Initialize the Chrome driver with options
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.set_window_size(1920, 1080)
+    driver.implicitly_wait(2)
+
+    # Open the Yahoo Finance page for AAPL
+    driver.get("https://finance.yahoo.com/quote/AAPL/")
+
+    handle_cookies(driver)
 
